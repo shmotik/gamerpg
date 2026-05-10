606407eaa30ae298ff9878a46e6f262e771fa794
KIS-118SIK <77wave778@gmail.com>
2026-05-10 16:01:22 +0300

    пытка1

--- scenes/menu.py
+++ scenes/menu.py
@@ -19,6 +19,8 @@ class Menu(Scene):
 
         self.update_fonts()
 
+        self.button_rects = []
+
         self.up_keys = [pygame.K_UP, pygame.K_w]
         self.down_keys = [pygame.K_DOWN, pygame.K_s]
 
@@ -28,6 +30,28 @@ class Menu(Scene):
     def update(self, screen, keys, events, dt=None):
 
         for event in events:
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+
+                if event.button == 1:
+
+                    mouse_pos = pygame.mouse.get_pos()
+
+                    for i, rect in enumerate(self.button_rects):
+
+                        if rect.collidepoint(mouse_pos):
+
+                            self.selected = i
+
+                            if self.buttons[i] == "START":
+                                return "game"
+
+                            if self.buttons[i] == "SETTINGS":
+                                return ("settings", "menu")
+
+                            if self.buttons[i] == "EXIT":
+                                return "exit"
+
             if event.type == pygame.KEYDOWN:
 
                 if event.key in self.up_keys:
@@ -62,14 +86,42 @@ class Menu(Scene):
         title = self.font.render("MY GAME", True, (255, 255, 255))
         screen.blit(title, (300, 100))
 
+        self.button_rects.clear()
+        mouse_pos = pygame.mouse.get_pos()
+
         for i, btn in enumerate(self.buttons):
 
-            color = (255, 255, 0) if i == self.selected else (200, 200, 200)
-
-            text = self.small.render(btn, True, color)
+            text = self.small.render(btn, True, (255,255,255))
 
             x = width // 2 - text.get_width() // 2
             y = height // 2 + i * 60
+
+            rect = pygame.Rect(
+                x - 20,
+                y - 10,
+                text.get_width() + 40,
+                text.get_height() + 20
+            )
+
+            self.button_rects.append(rect)
+
+            hovered = rect.collidepoint(mouse_pos)
+
+            # hover мышкой
+            if hovered:
+                self.selected = i
+
+            # цвет
+            color = (255,255,0) if i == self.selected else (200,200,200)
+
+            # фон кнопки
+            pygame.draw.rect(screen, (40,40,40), rect)
+
+            # рамка
+            pygame.draw.rect(screen, color, rect, 2)
+
+            text = self.small.render(btn, True, color)
+
             screen.blit(text, (x, y))
             
 
